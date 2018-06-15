124ff97f66bcd74fb29fde4ab3ed73a42bbb2c60
vhsharo <victor.haro@code3e.com>
2018-06-15 12:28:41 -0500

    Inicial commit

---
+++ src/main/java/com/domain/mylibrary/Car.java
@@ -0,0 +1,19 @@
+package com.domain.mylibrary;
+
+public class Car {
+
+    public int numberOfDoors;
+    public int numberOfCylinders;
+    public int currentSpeed;
+
+    public Car(){
+
+    }
+
+    public Car(int numberOfDoors, int numberOfCylinders, int currentSpeed) {
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfCylinders = numberOfCylinders;
+        this.currentSpeed = currentSpeed;
+    }
+
+}
